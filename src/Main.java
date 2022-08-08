28e6169857b1840d7c5a338f713cfbb48249729d
yonaswoldemichael <ybyonas@gmail.com>
2022-08-08 14:47:03 -0400

    retrieveing list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/Users/yonaswoldemichael/Documents/hangmanwords.txt"));
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+
     }
 }
